5e68875a40485abd9a947ffbe23664910f9f9b85
Vinicius-PR <vinicius_auto@outlook.com>
2024-09-03 07:50:59 -0300

    Started with yup validation

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,11 +1,33 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import { Request, Response } from "express"
+import * as yup from 'yup'
 
-interface City {
+interface CityProps {
   name: string
 }
 
-export const create = (req: Request<{}, {}, City>, res: Response) => {
+const bodyRequestValidation: yup.Schema<CityProps> = yup.object().shape({
+  name: yup.string().required().min(3)
+})
+
+export const create = async (req: Request<{}, {}, CityProps>, res: Response) => {
+  let validatedData: CityProps | undefined = undefined
+
+  try {
+    validatedData = await bodyRequestValidation.validate(req.body)
+  } catch (error) {
+    const yupError = error as yup.ValidationError
+
+    res.json({
+      errors: {
+        default: yupError.message
+      }
+    })
+  }
+
+  console.log(validatedData)
 
   return res.send("Created!")
+
+
 }
